73ca6d6729650d5466cf3e82485c2ddde9b6998a
DavidRincon12 <davidsantiagorb@ufps.edu.co>
2024-04-08 10:58:06 -0500

    Si

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.forms['loginForm'];
+
+form.addEventListener('submit', function handleFormSubmit(event){
+    event.preventDefault();
+
+    const email = form['email'].value;
+    const password = form['password'].value;
+
+});
